d1488d89e908200d4da4b2604907e70900cec134
mikhailovve90 <mikhailov.ve90@gmail.com>
2017-04-20 09:56:02 +0300

    пример создания ветки

--- digest.rb
+++ digest.rb
@@ -5,7 +5,8 @@ slv = STDIN.gets.chomp
 
 puts "Каким способом хотите шифровать :
 1 - MD5
-2 - SHA1"
+2 - SHA1
+3 - SHA2"
 vibor = STDIN.gets.to_i
 
 if(vibor == 1)
@@ -14,6 +15,9 @@ if(vibor == 1)
 elsif(vibor == 2)
   sha1 = Digest::SHA1.hexdigest(slv)#шифрование в sha1 фразы введённой ранее
   puts "Сообщение " + sha1
+elsif(vibor == 3)
+  sha2 = Digest::SHA2.hexdigest(slv)#шифрование в sha1 фразы введённой ранее
+  puts "Сообщение " + sha2
 else
   puts "Вы ввели неверный тип шифрования"
 end
