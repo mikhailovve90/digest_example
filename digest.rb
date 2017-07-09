d860512aed0df0c562e71396aa56f2748ca37f85
mikhailovve90 <mikhailov.ve90@gmail.com>
2017-07-09 14:32:43 +0400

    Update digest.rb

--- digest.rb
+++ digest.rb
@@ -8,11 +8,11 @@ puts "Каким способом хотите шифровать :
 2 - SHA1"
 vibor = STDIN.gets.to_i
 
-if(vibor == 1)
-  md5 = Digest::MD5.hexdigest(slv) # шифрование в md5 фразы введённой ранее
+if vibor == 1
+  md5 = Digest::MD5.hexdigest(slv)
   puts "Сообщение " + md5
-elsif(vibor == 2)
-  sha1 = Digest::SHA1.hexdigest(slv)#шифрование в sha1 фразы введённой ранее
+elsif vibor == 2
+  sha1 = Digest::SHA1.hexdigest(slv)
   puts "Сообщение " + sha1
 else
   puts "Вы ввели недопустимое значение"
